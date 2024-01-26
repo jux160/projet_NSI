ea0cd68a22b0c1eeb1b73dbc14f365c71afc33c8
jux160 <julien.gadotti1@gmail.com>
2024-01-26 22:46:58 +0100

    #

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return" Hello World"
+    return"<h1> Hello World</h1>"
 
 if __name__ == '__main__':
     app.run()

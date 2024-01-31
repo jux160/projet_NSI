6dab7e38e15fa71102fe5e299c02faa2675fc808
jux160 <julien.gadotti1@gmail.com>
2024-01-31 19:56:55 +0100

    a

--- app/models.py
+++ app/models.py
@@ -2,13 +2,14 @@ from flask_sqlalchemy import SQLAlchemy
 
 from .views import app
 
+# Create database connection object
 db = SQLAlchemy(app)
 
-class Content(db.model):
-    id= db.Column(db.Integer, primary_key=True)
-    description= db.Column(db.String(200), nullable=False)
-    gender =  db.Column(db.Integer(), nullable=False)
-    
+class Content(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(200), nullable=False)
+    gender = db.Column(db.Integer(), nullable=False)
+
     def __init__(self, description, gender):
         self.description = description
         self.gender = gender

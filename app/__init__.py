f99a120deca9c80eeebecbfc489b9909af212430
jux160 <julien.gadotti1@gmail.com>
2024-02-29 08:42:10 +0100

    f

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,3 @@ from flask import Flask
 
 from .views import app
 from . import models
-
-# Connect sqlalchemy to app
-models.db.init_app(app)
-
-@app.cli.command()
-def init_db():
-    models.init_db()
